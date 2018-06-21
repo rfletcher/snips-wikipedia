c6a6f9ef6426b4041c27f0dc44879270800ca672
Lars Martin <lars.martin@smb-tec.com>
2018-06-21 08:57:35 +0200

    add try/catch block

--- action-searchWikipedia.py
+++ action-searchWikipedia.py
@@ -3,6 +3,7 @@
 
 import re
 import io
+import sys
 
 import ConfigParser
 from hermes_python.hermes import Hermes
@@ -44,19 +45,24 @@ def action_wrapper(hermes, intentMessage, conf):
     if len(intentMessage.slots.article_indicator) > 0:
         article = intentMessage.slots.article_indicator.first().value
         wiki.set_lang('de')
-        results = wiki.search(article, 5)
-        lines = 2
-        summary = wiki.summary(results[0], lines)
-        if "==" in summary or len(summary) > 250:
-            # We hit the end of the article summary or hit a really long
-            # one.  Reduce to first line.
-            lines = 1
+        try:
+            results = wiki.search(article, 5)
+            lines = 2
             summary = wiki.summary(results[0], lines)
+            if "==" in summary or len(summary) > 250:
+                # We hit the end of the article summary or hit a really long
+                # one.  Reduce to first line.
+                lines = 1
+                summary = wiki.summary(results[0], lines)
 
-        summary = re.sub(r'\([^)]*\)|/[^/]*/', '', summary).encode('utf8')
-        hermes.publish_end_session(intentMessage.session_id, summary)
+            summary = re.sub(r'\([^)]*\)|/[^/]*/', '', summary).encode('utf8')
+            hermes.publish_end_session(intentMessage.session_id, summary)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            hermes.publish_end_session(intentMessage.session_id, "An error occured")
     else:
         hermes.publish_end_session(intentMessage.session_id, "An error occured")
+
 
 
 if __name__ == "__main__":
