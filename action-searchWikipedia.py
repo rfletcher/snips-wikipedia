24e1b3fa2473177f670b25173025089288338353
Rick Fletcher <fletch@pobox.com>
2019-09-03 17:22:15 -0400

    Formatting

--- action-searchWikipedia.py
+++ action-searchWikipedia.py
@@ -13,11 +13,9 @@ import wikipedia as wiki
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
 
 def read_configuration_file(configuration_file):
     try:
@@ -28,15 +26,12 @@ def read_configuration_file(configuration_file):
     except (IOError, ConfigParser.Error) as e:
         return dict()
 
-
 def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
     action_wrapper(hermes, intentMessage, conf)
 
-
 def action_wrapper(hermes, intentMessage, conf):
     """
-
     :param hermes:
     :param intentMessage:
     :param conf:
@@ -63,9 +58,6 @@ def action_wrapper(hermes, intentMessage, conf):
     else:
         hermes.publish_end_session(intentMessage.session_id, "An error occured")
 
-
-
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("CrystalMethod:searchWikipedia", subscribe_intent_callback) \
-         .start()
+        h.subscribe_intent("rfletcher:searchWikipedia", subscribe_intent_callback).start()
