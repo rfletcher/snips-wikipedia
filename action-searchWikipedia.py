1779ae23938eec289765461ba9aaac1c8379afcd
Rick Fletcher <fletch@pobox.com>
2019-09-03 21:53:26 -0400

    Ditch wikipedia config support

--- action-searchWikipedia.py
+++ action-searchWikipedia.py
@@ -2,39 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import re
-import io
 import sys
 
-import ConfigParser
 from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
 import wikipedia as wiki
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
-
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
-
-def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
-
-def action_wrapper(hermes, intentMessage, conf):
+def subscribe_intent_searchWikipedia(hermes, intentMessage):
     """
     :param hermes:
     :param intentMessage:
-    :param conf:
     :return:
     """
     if len(intentMessage.slots.article_indicator) > 0:
@@ -59,5 +37,6 @@ def action_wrapper(hermes, intentMessage, conf):
         hermes.publish_end_session(intentMessage.session_id, "An error occured")
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        h.subscribe_intent("rfletcher:searchWikipedia", subscribe_intent_callback).start()
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("rfletcher:searchWikipedia", subscribe_intent_searchWikipedia).loop_forever()
